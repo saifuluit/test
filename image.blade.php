c422e759dc443a51d5412eb80a2951f6a01b7bc8
saifulbashar <saifulsojib43@gmail.com>
2019-02-01 22:22:19 +0600

    first

---
+++ image.blade.php
@@ -0,0 +1,18 @@
+public function productStore(Request $request)
+    {
+    	# code...
+    	$product = new Product();
+    	if ($request->hasFile('image'))
+		{
+	        $file = $request->file('image');
+	        $name = time() . '_product_' . $file->getClientOriginalName();
+	        $file->move('product/', $name);
+	        $product->image = $name;
+    	}
+    	$product->name = $request->name;
+    	$product->category_id = $request->category_id;
+    	$product->save();
+    	return redirect()->back();
+    }
+
+    //nn
